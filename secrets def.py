fc5a4ad40c677d33f99b88ae879917727056394e
ItsNotGoodName <gurnaindeol@gmail.com>
2020-08-29 14:09:57 -0700

    fix

--- secrets def.py
+++
@@ -1,7 +0,0 @@
-# Copy this file to secrets.py
-
-MAILBOX_FOLDER="/path/to/mail/folder"
-MAILBOX_NAME="name-of-mailbox-in-mailfolder"
-CHAT_ID="telegram-channel-id-use-the-telegram-browser-app-to-figure-it-out"
-TOKEN="YOUR-BOT-TOKEN-FROM-BOT-FATHER"
-ENV="production" # set it to anything other than production to make it development
