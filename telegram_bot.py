e8c76f764297ec00b79bf5d7294e9bb454564cce
ItsNotGoodName <gurnaindeol@gmail.com>
2020-08-29 13:18:55 -0700

    don't store chat_id

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,11 +4,9 @@ import logging
 from time import time, sleep
 
 class TelegramBot():
-    def __init__(self, token, seconds_between_messages, chat_id):
+    def __init__(self, token, seconds_between_messages):
         self.last_message_time = 0
         self.seconds_between_messages = seconds_between_messages
-        self.chat_id = chat_id
-
         self.updater = Updater(token=token, use_context=True)
         self.updater.dispatcher.add_handler(CommandHandler("ping", self._ping))
         self.updater.start_polling()
@@ -16,20 +14,20 @@ class TelegramBot():
     def _ping(self, update, context):
        update.message.reply_text('pong', disable_notification=True) 
 
-    def send_message(self, message, disable_notification=False):
+    def send_message(self, message, chat_id, disable_notification=False):
         if(time() - self.last_message_time < self.seconds_between_messages):
             logging.debug(f"Sleeping for {self.seconds_between_messages}")
             sleep(self.seconds_between_messages)
 
         try:
-            self.updater.bot.sendMessage(chat_id=self.chat_id, text=message, disable_notification=disable_notification)
+            self.updater.bot.sendMessage(chat_id=chat_id, text=message, disable_notification=disable_notification)
         except Exception: # TODO: Be specific on on exceptions
             logging.error("Too Much Spam")
 
         self.last_message_time = time()
 
 
-    def send_photos(self, caption, paths):
+    def send_photos(self, caption, paths, chat_id):
         if(time() - self.last_message_time < self.seconds_between_messages):
             logging.debug(f"Sleeping for {self.seconds_between_messages}")
             sleep(self.seconds_between_messages)
@@ -37,14 +35,14 @@ class TelegramBot():
         try:
             num_paths = len(paths)
             if(num_paths == 1):
-                self.updater.bot.send_photo(chat_id=self.chat_id, photo=open(paths[0], 'rb'), caption=caption)
+                self.updater.bot.send_photo(chat_id=chat_id, photo=open(paths[0], 'rb'), caption=caption)
             elif(num_paths >= 2 and num_paths <= 10):
                 media = []
                 for path in paths:
                     media.append(InputMediaPhoto(open(path, "rb")))
 
                 media[0].caption = caption
-                self.updater.bot.sendMediaGroup(chat_id=self.chat_id, media=media)
+                self.updater.bot.sendMediaGroup(chat_id=chat_id, media=media)
             else:
                 logging.error(f"paths length is too small or too big, length {str(num_paths)}")
         except Exception: # TODO: Be specific on on exceptions
