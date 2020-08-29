096a4d1ccfeb31b9d14170d6865469728682e866
ItsNotGoodName <gurnaindeol@gmail.com>
2020-08-29 13:17:12 -0700

    Change to ini parser

--- constants.py
+++ constants.py
@@ -1,12 +1,29 @@
 import os
 import secrets
 
-PICTURE_PATH=os.path.join(os.getcwd(), "attachments")
-MAILBOX_PATH=os.path.join(secrets.MAILBOX_FOLDER, secrets.MAILBOX_NAME)
-SECONDS_BETWEEN_MESSAGES=5
+import configparser
 
-MAILBOX_FOLDER=secrets.MAILBOX_FOLDER
-MAILBOX_NAME=secrets.MAILBOX_NAME
-CHAT_ID=secrets.CHAT_ID
-TOKEN=secrets.TOKEN
-ENV=secrets.ENV
+MAIL_FOLDER = MAIL_FILE = MAIL_PATH = ATTACHMENTS_FOLDER = MESSAGE_TIMEOUT = TOKEN = ENV = TRANSFERS = None
+
+def load_config(file_name):
+    global MAIL_FOLDER , MAIL_FILE , MAIL_PATH , ATTACHMENTS_FOLDER , MESSAGE_TIMEOUT , TOKEN , ENV , TRANSFERS
+
+    config = configparser.ConfigParser()
+    config.read(file_name)
+
+    MAIL_FOLDER         = config['DEFAULT']['mail_folder']
+    MAIL_FILE           = config['DEFAULT']['mail_file']
+    MAIL_PATH           = os.path.join(MAIL_FOLDER, MAIL_FILE)
+    ATTACHMENTS_FOLDER  = config['DEFAULT']['attachments_folder']
+    MESSAGE_TIMEOUT     = int(config['DEFAULT']['message_timeout'])
+    TOKEN               = config['DEFAULT']['token']
+    ENV                 = config['DEFAULT']['env']
+
+    TRANSFERS = []
+    for section in config.sections():
+        chat_id = config[section]["chat_id"]
+        from_address = config[section]["from_address"]
+        to_address = config[section]["to_address"]
+        TRANSFERS.append({"chat_id": chat_id, "from_address": from_address,"to_address": to_address})
+
+load_config('config.ini')
