f71f931a7cdc979fd76bbf0caecbdfe0d561d520
ItsNotGoodName <gurnaindeol@gmail.com>
2021-02-19 16:35:04 -0800

    Add telegram_bot

--- email_to_telegram/__main__.py
+++ email_to_telegram/__main__.py
@@ -1,14 +1,14 @@
 import logging
 
-from .constants import (
-    ENV,
-)
+from .constants import ENV, MESSAGE_TIMEOUT, TOKEN, TRANSFERS
+from .telegram_bot import TelegramBot
 
 
 def main():
     if ENV != "production":
         logging.basicConfig(level=logging.DEBUG)
     logging.debug("Entered main")
+    bot = TelegramBot(TOKEN, MESSAGE_TIMEOUT)
 
 
 if __name__ == "__main__":
---
+++ email_to_telegram/telegram_bot.py
@@ -0,0 +1,63 @@
+import logging
+
+from time import time, sleep
+from telegram import InputMediaPhoto
+from telegram.ext import Updater, CommandHandler
+from telegram.error import TelegramError
+
+
+def api_interaction(func):
+    def decorator(*args, **kwargs):
+        self = args[0]
+        if time() - self.last_message_time < self.message_timeout:
+            logging.info(f"Sleeping for {self.message_timeout}")
+            sleep(self.message_timeout)
+
+        try:
+            func(*args, **kwargs)
+        except TelegramError as e:
+            logging.error(e)
+
+        self.last_message_time = time()
+
+    return decorator
+
+
+class TelegramBot:
+    def __init__(self, token, message_timeout):
+        self.last_message_time = 0
+        self.message_timeout = message_timeout
+        self.updater = Updater(token=token, use_context=True)
+        self.updater.dispatcher.add_handler(CommandHandler("ping", self._ping))
+        self.updater.start_polling()
+
+    def _ping(self, update, context):
+        update.message.reply_text("pong")
+        logging.debug("Replied to ping")
+
+    @api_interaction
+    def send_message(self, chat_id, message, disable_notification=False):
+        self.updater.bot.sendMessage(
+            chat_id=chat_id, text=message, disable_notification=disable_notification
+        )
+        logging.debug("Sent message")
+
+    @api_interaction
+    def send_photos(self, chat_id, paths, caption=""):
+        length = len(paths)
+        if length == 1:
+            self.updater.bot.send_photo(
+                chat_id=chat_id, photo=open(paths[0], "rb"), caption=caption
+            )
+        elif 2 <= length <= 10:
+            media = []
+            for path in paths:
+                media.append(InputMediaPhoto(open(path, "rb")))
+
+            media[0].caption = caption
+            self.updater.bot.sendMediaGroup(chat_id=chat_id, media=media)
+        else:
+            logging.error(
+                "paths length is too small or too big, length %s", str(length)
+            )
+        logging.debug("Sent pictures")
