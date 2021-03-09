2b839aa069dd5b0cd4c8c25f6ee417c6f32937dd
ItsNotGoodName <35015993+ItsNotGoodName@users.noreply.github.com>
2021-03-08 20:15:29 -0800

    fix text format for caption

--- email_to_telegram/email.py
+++ email_to_telegram/email.py
@@ -45,7 +45,7 @@ def consume_emails(emails, bot):
                 bot.send_photo(
                     transfer["chat_id"],
                     attachment_paths,
-                    email.subject + "\n" + str(email.text_plain),
+                    email.subject + "\n" + "".join(email.text_plain),
                 )
             elif not transfer["disable_text"]:
                 bot.send_text(
