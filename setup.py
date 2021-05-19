39c19ccc9298aa9fe40337211ed37b8e2287f7fd
ItsNotGoodName <gurnaindeol@gmail.com>
2021-05-18 22:23:08 -0700

    specify dependency versions

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="email-to-telegram",
-    version="1.1.0",
+    version="1.1.1",
     description="Telegram bot that reads a mail file and forwards to Telegram chat",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -16,7 +16,7 @@ setup(
     author_email="gurnaindeol@gmail.com",
     packages=find_packages(exclude="test"),
     python_requires=">=3.6, <4",
-    install_requires=["python-telegram-bot", "watchdog", "mail-parser"],
+    install_requires=["python-telegram-bot>=13.5,<14.0", "watchdog>=2.1.1,<3.0.0", "mail-parser>=3.15.0,<4.0.0"],
     entry_points={
         "console_scripts": [
             "email-to-telegram=email_to_telegram.__main__:main",
